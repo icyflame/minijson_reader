93aaaa6b60dfb8b9e47f26f5adb25aa4788ad8ae
Siddharth Kannan <mail@siddharthkannan.in>
2019-09-23 10:07:00 +0900

    Rename to cpp files so they can build to a binary

--- examples/main-finder.cpp
+++ examples/main-finder.cpp
@@ -11,10 +11,11 @@ int main(int argc, char* argv[]) {
 
     const char* json_obj = file_contents.c_str();
 
-    nested_json::finder finder(
-        json_obj, strlen(json_obj)-1, 8150);
+    int offset = atoi(argv[2]);
 
-    std::string want_path = finder.start();
+    nested_json::finder finder_obj(json_obj, strlen(json_obj)-1, offset);
+
+    std::string want_path = finder_obj.start();
 
     std::cout << want_path << std::endl;
 
